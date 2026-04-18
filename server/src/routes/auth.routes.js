f48064ca353dc07b03c0a75833fc0f6e6de0ff65
htuanqn <phamhoangtuanqn@gmail.com>
2026-04-18 07:27:29 +0700

    feat: backend api for facebook verifying Co-authored-by: maaitlunghau <chunhau.py@gmail.com>

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { register, login, me, logout } from '../controllers/auth.controller.js';
 import { googleLogin, getGoogleConfig } from '../controllers/googleAuth.controller.js';
+import { facebookLogin, getFacebookConfig } from '../controllers/facebookAuth.controller.js';
 import { authenticate } from '../middleware/auth.middleware.js';
 
 const router = Router();
@@ -12,5 +13,8 @@ router.post('/logout', authenticate, logout);
 
 router.post('/google', googleLogin);
 router.get('/google-config', getGoogleConfig);
+
+router.post('/facebook', facebookLogin);
+router.get('/facebook-config', getFacebookConfig);
 
 export default router;
